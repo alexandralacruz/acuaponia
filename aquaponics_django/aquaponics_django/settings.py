9c4a0e1e148d95d2acc7873e4e94ceb172a3f362
alexandra la cruz <alexandra.lacruz@gmail.com>
2022-12-13 10:35:04 -0500

    Running and npm run build was done

--- aquaponics_django/aquaponics_django/settings.py
+++ aquaponics_django/aquaponics_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^6ekph@1s&@o_9mknq=n8fj%-@#5t^=4ayenw*6-3)izaag@hu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [http://localhost:8080]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'webpack_loader'
+
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
@@ -48,14 +47,6 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
 ]
-
-
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'dist/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-    }
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
