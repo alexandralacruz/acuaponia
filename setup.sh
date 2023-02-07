91cdad3c87aa147d58e2591aa7524765a7d26226
alexandra la cruz <alexandra.lacruz@gmail.com>
2023-02-07 09:30:22 -0500

    setup amb virtual

--- setup.sh
+++ setup.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
-cd ../aquaponics_django
+cd ..
+
+# Instalar pipenv
+pip install pipenv
+
+# Crear entorno virtual con pipenv
+pipenv install
+
+# Activar entorno virtual
+pipenv shell
+
+# Instalar dependencias para el backend de Django
+pip install -r requirements.txt
+
+# Realizar migraciones en la base de datos
+cd aquaponics_django
 python manage.py migrate
+
+# Correr el servidor de Django
 python manage.py runserver &
-cd ../aquaponics_vue
-npm run serve
+
+# Correr el servidor de Vue
+cd ../aquaponics_vue &
+npm run serve &
+
+# Esperar a que ambos procesos finalicen
+wait
