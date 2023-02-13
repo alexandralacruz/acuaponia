2559181154f7ae5049925f0b54dfc3a4e34e696d
alexandra la cruz <alexandra.lacruz@gmail.com>
2023-02-13 11:01:45 -0500

    PLEASE PLEASE

--- aquaponics_vue/src/main.js
+++ aquaponics_vue/src/main.js
@@ -4,5 +4,5 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'http://alexandralacruzgm.pythonanywhere.com/'
 createApp(App).use(store).use(router).mount('#app')
